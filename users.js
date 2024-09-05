73a37760aafae33b0c32ef376c1e4b9cfd02a5f4
Drakhur <68760829+Drakhur@users.noreply.github.com>
2024-09-05 16:50:18 -0700

    work on user late, and phots changing to Mongodb

--- users.js
+++ users.js
@@ -7,10 +7,3 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
-// TODO: Add validation for the following fields:
-/*
-- Username must be between 3 and 20 characters long
-- Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (e.g., !
-- Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (e.g., !.
-- Password must be at least 8 characters long
-*/
