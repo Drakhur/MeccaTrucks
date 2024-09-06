22de7a522f7b177d83c699cc56ba785ce6d209dd
Drakhur <68760829+Drakhur@users.noreply.github.com>
2024-09-05 17:46:04 -0700

    changes

--- home.js
+++ home.js
@@ -7,7 +7,7 @@ fabars.onclick = () => {
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    fetch("../resources.json")
+    fetch("./client/resources.json")
     .then(response => response.json())
     .then( data => {
 
@@ -29,43 +29,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
     
 });
-// let CurrContainerIndex = 0;
-//     const containers = Array.from(document.querySelectorAll('.container'));
-//     let isTransitioning = false;
 
-        //     // Add scroll event listener
-        // window.addEventListener('scroll', function() {
-        //     const scrollY = window.scrollY || document.documentElement.scrollTop;
-        //     const threshhold = 40;
-
-        //     // Trigger transition when scrolled past a certain point
-        //     if (scrollY > threshhold && CurrContainerIndex < containers.length -1) {
-        //         transitionContainers(CurrContainerIndex + 1);
-        //     }
-        //     // Trigger transition when scrolled back to a certain point
-        //     if (scrollY < threshhold && CurrContainerIndex > 0) {
-        //         transitionContainers(CurrContainerIndex - 1);
-        //     }
-        // });
-        // // function to transition containers
-        // function transitionContainers(nextIndex) {
-        //     // Get the current container index
-        //     const CurrContainer = containers[CurrContainerIndex];
-        //     const nextContainer = containers[CurrContainerIndex];
-        //     // Check if the next container is in view
-
-        //     if(nextContainer){
-        //         // fade out the current container
-        //         CurrContainer.classList.add("hidden");
-
-        //         // wait for the fade to finish
-        //         setTimeout(() => {
-        //             CurrContainer.classList.add('final'); // set display to nnoe
-        //             nextContainer.classList.remove('hidden'); //start fading in the next one
-        //             isTransitioning = false;
-
-        //         }, 2000); //apparently matches opacity transition time
-        //         currentContainerIndex = nextIndex;
-        //     }
-        // }
    
